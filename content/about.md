0d238f52070bacc158a2b916569acb8ea663f885
Tobias Ibounig <tobijdc@users.noreply.github.com>
2024-01-28 16:13:23 +0100

    with countdown

--- content/about.md
+++ content/about.md
@@ -4,4 +4,8 @@ author = "Tobijdc"
 draft = false
 +++
 
-teeeeeeeeeeest
+Guess the SC2 Player.
+You have to guess the SC2 player in 5 tries.
+With every try you will see how the guessed player compares to the target player.
+
+Have fun.
--- static/js/game.js
+++ static/js/game.js
@@ -98,10 +98,485 @@ var players = [
         "race": "Z",
         "rating": 2.2307688124170144,
         "position": 9
+    },
+    {
+        "id": 1665,
+        "tag": "Cure",
+        "name": "김도욱",
+        "romanized_name": "Kim Doh Wook",
+        "birthday": "1994-11-25",
+        "country": "KR",
+        "race": "T",
+        "rating": 2.17881364841307,
+        "position": 10
+    },
+    {
+        "id": 258,
+        "tag": "HeRoMaRinE",
+        "name": "Gabriel Segat",
+        "romanized_name": "",
+        "birthday": "1997-07-04",
+        "country": "DE",
+        "race": "T",
+        "rating": 2.02140775117387,
+        "position": 11
+    },
+    {
+        "id": 10115,
+        "tag": "Oliveira",
+        "name": "李培楠",
+        "romanized_name": "Li Peinan",
+        "birthday": "2000-06-28",
+        "country": "CN",
+        "race": "T",
+        "rating": 2.01894005647133,
+        "position": 12
+    },
+    {
+        "id": 117,
+        "tag": "SHIN",
+        "name": "신희범",
+        "romanized_name": "Shin Hee Bum",
+        "birthday": "1996-09-03",
+        "country": "KR",
+        "race": "Z",
+        "rating": 2.00373511506289,
+        "position": 13
+    },
+    {
+        "id": 1662,
+        "tag": "Rogue",
+        "name": "이병렬",
+        "romanized_name": "Lee Byung Ryul",
+        "birthday": "1994-01-13",
+        "country": "KR",
+        "race": "Z",
+        "rating": 1.97821094086991,
+        "position": "NULL"
+    },
+    {
+        "id": 44,
+        "tag": "GuMiho",
+        "name": "고병재",
+        "romanized_name": "Koh Byung Jae",
+        "birthday": "1992-08-04",
+        "country": "KR",
+        "race": "T",
+        "rating": 1.97785509782719,
+        "position": 14
+    },
+    {
+        "id": 4495,
+        "tag": "Neeb",
+        "name": "Alex Sunderhaft",
+        "romanized_name": "NULL",
+        "birthday": "1998-02-17",
+        "country": "US",
+        "race": "P",
+        "rating": 1.96548320564563,
+        "position": "NULL"
+    },
+    {
+        "id": 186,
+        "tag": "Classic",
+        "name": "김도우",
+        "romanized_name": "Kim Doh Woo",
+        "birthday": "1991-11-27",
+        "country": "KR",
+        "race": "P",
+        "rating": 1.95940014995287,
+        "position": 15
+    },
+    {
+        "id": 1658,
+        "tag": "Zest",
+        "name": "주성욱",
+        "romanized_name": "Joo Sung Wook",
+        "birthday": "1992-07-11",
+        "country": "KR",
+        "race": "P",
+        "rating": 1.94419936975125,
+        "position": "NULL"
+    },
+    {
+        "id": 5,
+        "tag": "PartinG",
+        "name": "원이삭",
+        "romanized_name": "Won Lee Sak",
+        "birthday": "1994-08-24",
+        "country": "KR",
+        "race": "P",
+        "rating": 1.8964629507308128,
+        "position": 16
+    },
+    {
+        "id": 4,
+        "tag": "DRG",
+        "name": "박수호",
+        "romanized_name": "Park Soo Ho",
+        "birthday": "1991-06-03",
+        "country": "KR",
+        "race": "Z",
+        "rating": 1.89353037356608,
+        "position": 17
+    },
+    {
+        "id": 2,
+        "tag": "Creator",
+        "name": "장현우",
+        "romanized_name": "Jang Hyun Woo",
+        "birthday": "1997-02-23",
+        "country": "KR",
+        "race": "P",
+        "rating": 1.87768583505543,
+        "position": 18
+    },
+    {
+        "id": 2170,
+        "tag": "ShoWTimE",
+        "name": "Tobias Sieber",
+        "romanized_name": "NULL",
+        "birthday": "1994-02-23",
+        "country": "DE",
+        "race": "P",
+        "rating": 1.8747519420518,
+        "position": 19
+    },
+    {
+        "id": 2053,
+        "tag": "Zoun",
+        "name": "박한솔",
+        "romanized_name": "Park Han Sol",
+        "birthday": "1997-06-17",
+        "country": "KR",
+        "race": "P",
+        "rating": 1.84513764483654,
+        "position": "NULL"
+    },
+    {
+        "id": 4049,
+        "tag": "Lambo",
+        "name": "Julian Brosig",
+        "romanized_name": "NULL",
+        "birthday": "1995-11-10",
+        "country": "DE",
+        "race": "Z",
+        "rating": 1.83866523598793,
+        "position": 20
+    },
+    {
+        "id": 1517,
+        "tag": "Bunny",
+        "name": "이재선",
+        "romanized_name": "Lee Jae Sun",
+        "birthday": "1995-09-11",
+        "country": "KR",
+        "race": "T",
+        "rating": 1.83722989008745,
+        "position": 21
+    },
+    {
+        "id": 8148,
+        "tag": "NightMare",
+        "name": "장욱",
+        "romanized_name": "Jang Wook",
+        "birthday": "1999-02-23",
+        "country": "KR",
+        "race": "P",
+        "rating": 1.83307557052765,
+        "position": 22
+    },
+    {
+        "id": 177,
+        "tag": "Trap",
+        "name": "조성호",
+        "romanized_name": "Cho Sung Ho",
+        "birthday": "1994-03-24",
+        "country": "KR",
+        "race": "P",
+        "rating": 1.82451812662498,
+        "position": "NULL"
+    },
+    {
+        "id": 8676,
+        "tag": "SKillous",
+        "name": "Никита Гуревич",
+        "romanized_name": "Nikita Gurevich",
+        "birthday": "2001-04-17",
+        "country": "RU",
+        "race": "P",
+        "rating": 1.81465039306422,
+        "position": 23
+    },
+    {
+        "id": 4134,
+        "tag": "Astrea",
+        "name": "Max Angel",
+        "romanized_name": "NULL",
+        "birthday": "1997-03-08",
+        "country": "US",
+        "race": "P",
+        "rating": 1.80516552615102,
+        "position": 24
+    },
+    {
+        "id": 5847,
+        "tag": "Elazer",
+        "name": "Mikołaj Ogonowski",
+        "romanized_name": "NULL",
+        "birthday": "1997-12-11",
+        "country": "PL",
+        "race": "Z",
+        "rating": 1.79657975263624,
+        "position": 25
+    },
+    {
+        "id": 48,
+        "tag": "INnoVation",
+        "name": "이신형",
+        "romanized_name": "Lee Shin Hyung",
+        "birthday": "1993-07-25",
+        "country": "KR",
+        "race": "T",
+        "rating": 1.79029793001714,
+        "position": "NULL"
+    },
+    {
+        "id": 4452,
+        "tag": "Spirit",
+        "name": "Piotr Walukiewicz",
+        "romanized_name": "NULL",
+        "birthday": "1998-10-08",
+        "country": "PL",
+        "race": "T",
+        "rating": 1.78770245061966,
+        "position": 26
+    },
+    {
+        "id": 309,
+        "tag": "Stats",
+        "name": "김대엽",
+        "romanized_name": "Kim Dae Yeob",
+        "birthday": "1992-05-15",
+        "country": "KR",
+        "race": "P",
+        "rating": 1.75410795985638,
+        "position": 27
+    },
+    {
+        "id": 23,
+        "tag": "Scarlett",
+        "name": "Sasha Hostyn",
+        "romanized_name": "NULL",
+        "birthday": "1993-12-14",
+        "country": "CA",
+        "race": "Z",
+        "rating": 1.71825981082724,
+        "position": 28
+    },
+    {
+        "id": 35,
+        "tag": "Ryung",
+        "name": "김동원",
+        "romanized_name": "Kim Dong Won",
+        "birthday": "1991-04-16",
+        "country": "KR",
+        "race": "T",
+        "rating": 1.69274222798226,
+        "position": 29
+    },
+    {
+        "id": 23496,
+        "tag": "Reynor",
+        "name": "Riccardo Romiti",
+        "romanized_name": "NULL",
+        "birthday": "2002-07-01",
+        "country": "IT",
+        "race": "S",
+        "rating": 1.68806233741458,
+        "position": 30
+    },
+    {
+        "id": 109,
+        "tag": "Dream",
+        "name": "조중혁",
+        "romanized_name": "Cho Joong Hyuk",
+        "birthday": "1996-10-27",
+        "country": "KR",
+        "race": "T",
+        "rating": 1.68564353418834,
+        "position": "NULL"
+    },
+    {
+        "id": 13361,
+        "tag": "Firefly",
+        "name": "薛涛",
+        "romanized_name": "Xue Tao",
+        "birthday": "2000-10-02",
+        "country": "CN",
+        "race": "P",
+        "rating": 1.68532424035139,
+        "position": 31
+    },
+    {
+        "id": 4105,
+        "tag": "Kelazhur",
+        "name": "Diego Guilherme Schwimer",
+        "romanized_name": "NULL",
+        "birthday": "1995-10-10",
+        "country": "BR",
+        "race": "T",
+        "rating": 1.65978894764392,
+        "position": 32
+    },
+    {
+        "id": 184,
+        "tag": "SpeCial",
+        "name": "Juan Carlos Tena Lopez",
+        "romanized_name": "NULL",
+        "birthday": "1993-05-20",
+        "country": "MX",
+        "race": "T",
+        "rating": 1.64053264529104,
+        "position": 33
+    },
+    {
+        "id": 13216,
+        "tag": "Wayne",
+        "name": "Иван Чепурнов",
+        "romanized_name": "Ivan Chepurnov",
+        "birthday": "2001-06-09",
+        "country": "RU",
+        "race": "Z",
+        "rating": 1.63011138147428,
+        "position": 34
+    },
+    {
+        "id": 1659,
+        "tag": "Dear",
+        "name": "백동준",
+        "romanized_name": "Baek Dong Jun",
+        "birthday": "1994-04-25",
+        "country": "KR",
+        "race": "P",
+        "rating": 1.62206873829022,
+        "position": "NULL"
+    },
+    {
+        "id": 110,
+        "tag": "sOs",
+        "name": "김유진",
+        "romanized_name": "Kim Yoo Jin",
+        "birthday": "1993-10-16",
+        "country": "KR",
+        "race": "P",
+        "rating": 1.60425570546937,
+        "position": "NULL"
+    },
+    {
+        "id": 214,
+        "tag": "Harstem",
+        "name": "Kevin de Koning",
+        "romanized_name": "NULL",
+        "birthday": "1994-07-28",
+        "country": "NL",
+        "race": "P",
+        "rating": 1.59622911990081,
+        "position": 35
+    },
+    {
+        "id": 63,
+        "tag": "TY",
+        "name": "전태양",
+        "romanized_name": "Jun Tae Yang",
+        "birthday": "1994-09-18",
+        "country": "KR",
+        "race": "T",
+        "rating": 1.58295973078416,
+        "position": "NULL"
+    },
+    {
+        "id": 1171,
+        "tag": "Strange",
+        "name": "Алексей Соляков",
+        "romanized_name": "Alexey Solyakov",
+        "birthday": "1997-03-04",
+        "country": "RU",
+        "race": "P",
+        "rating": 1.56306953896648,
+        "position": 36
+    },
+    {
+        "id": 1649,
+        "tag": "MarineLorD",
+        "name": "Alexis Eusebio",
+        "romanized_name": "NULL",
+        "birthday": "1995-06-01",
+        "country": "FR",
+        "race": "T",
+        "rating": 1.55301982095323,
+        "position": "NULL"
+    },
+    {
+        "id": 125,
+        "tag": "soO",
+        "name": "어윤수",
+        "romanized_name": "Eo Yun Su",
+        "birthday": "1992-09-24",
+        "country": "KR",
+        "race": "Z",
+        "rating": 1.5515491579998992,
+        "position": 37
+    },
+    {
+        "id": 24,
+        "tag": "KeeN",
+        "name": "황규석",
+        "romanized_name": "Hwang Kyu Seok",
+        "birthday": "1994-10-02",
+        "country": "KR",
+        "race": "T",
+        "rating": 1.52073096428019,
+        "position": 38
+    },
+    {
+        "id": 300,
+        "tag": "FanTaSy",
+        "name": "정명훈",
+        "romanized_name": "Jung Myung Hoon",
+        "birthday": "1991-07-01",
+        "country": "KR",
+        "race": "T",
+        "rating": 1.50526335934731,
+        "position": "NULL"
+    },
+    {
+        "id": 58,
+        "tag": "MaNa",
+        "name": "Grzegorz Komincz",
+        "romanized_name": "NULL",
+        "birthday": "1993-12-14",
+        "country": "PL",
+        "race": "P",
+        "rating": 1.49325498486052,
+        "position": 39
+    },
+    {
+        "id": 4370,
+        "tag": "GunGFuBanDa",
+        "name": "Fabian Mayer",
+        "romanized_name": "NULL",
+        "birthday": "1998-04-23",
+        "country": "DE",
+        "race": "P",
+        "rating": 1.48137238607535,
+        "position": 40
     }
 ]
 
-var target = players[3];
+var today = new Date();
+var target = players[Math.floor(Math.random() * players.length)];
+var number_of_guesses = 5;
 
 function stats(guess, actual) {
     var result = "<b>" + guess.tag + "</b><br/>";
@@ -121,11 +596,17 @@ function stats(guess, actual) {
 }
 
 function guess(index) {
+    number_of_guesses--;
+    document.getElementById('number-of-tries').innerHTML = number_of_guesses;
     if (players[index].id == target.id) {
         document.getElementById('result-display').innerHTML = "winwinwin <br /><b>" + target.tag;
         alert('winwinwin');
+        return;
     } else {
         stats(players[index], target);
+    }
+    if (number_of_guesses <= 0) {
+        alert('sorry you lost. Target player was: ' + target.tag)
     }
 }
 
